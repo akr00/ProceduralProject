bcb059bb936227631691f72ba62b849b2c360907
akr00 <akridgeway7562@eagle.fgcu.edu>
2019-05-15 12:49:39 -0400

    first menu make

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 using namespace std;
-//hi github test
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Production Line Tracker\n" << endl;
+    cout << "1. Add Employee Account" << endl;
+    cout << "2. Add Music Player" << endl;
+    cout << "3. Add Movie Player" << endl;
+    cout << "4. Display Production Statistics" << endl;
+    cout << "5. Exit" << endl;
     return 0;
 }
