2c79cda31637c88993d749ec2d417f98b5b74182
aidenridgeway <akridgeway7562@eagle.fgcu.edu>
2019-06-21 09:46:00 -0400

    add file

---
+++ cmake-build-debug/CMakeFiles/ProcedualProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProcedualProject.dir/main.cpp.o"
+  "ProcedualProject.pdb"
+  "ProcedualProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProcedualProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
